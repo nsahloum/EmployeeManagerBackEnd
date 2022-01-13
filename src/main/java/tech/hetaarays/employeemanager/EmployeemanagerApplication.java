851de3d46810997b8bbeeee2ec0d8dc14db88b81
Najima Sahloumi <nsahloum@gmail.com>
2022-01-13 15:03:54 +0100

    add cross origin

--- src/main/java/tech/hetaarays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/hetaarays/employeemanager/EmployeemanagerApplication.java
@@ -1,7 +1,13 @@
 package tech.hetaarays.employeemanager;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
@@ -9,5 +15,6 @@ public class EmployeemanagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeemanagerApplication.class, args);
     }
+
 
 }
--- src/main/java/tech/hetaarays/employeemanager/api/EmployeeController.java
+++ src/main/java/tech/hetaarays/employeemanager/api/EmployeeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
